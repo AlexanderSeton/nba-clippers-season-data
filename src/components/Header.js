dbdf39da4b303175c8093459e70ac2c745e9c1d1
Alexander Seton <alexanderseton@protonmail.com>
2021-12-21 13:12:45 +0000

    responsive dropdown icon now appears when screen is below specified width.

--- src/components/Header.js
+++ src/components/Header.js
@@ -1,11 +1,20 @@
 import React from "react";
 import { NavLink } from "react-router-dom";
 import "./static/Header.css";
+import menuIcon from "../assets/menuIcon.svg";
 
 const Header = function() {
+
+    const dropDown = function() {
+        console.log("entered method");
+    }
+
     return(
         <div className="header">
             <nav className="nav-links">
+                <a href="javascript:void(0);" class="menu-icon" onclick={dropDown()}>
+                    <img src={menuIcon} alt="Menu Icon" />
+                </a>
                 <div className="nav-link">
                     <NavLink to="/" style={{ textDecoration: 'none' }}>
                         <h1 className="nav-link-h1">Games</h1>
@@ -22,9 +31,9 @@ const Header = function() {
                     </NavLink>
                 </div>
             </nav>
-            <div className="main-title">
+            {/* <div className="main-title">
                 <h1>Clipper's Team Data (2020/21)</h1>
-            </div>
+            </div> */}
         </div>
     )   
 }
