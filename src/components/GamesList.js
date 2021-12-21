a3734da17a7af3607830565a59549db975e5a59d
Alexander Seton <alexanderseton@protonmail.com>
2021-12-21 21:15:38 +0000

    Added responsive styling for mobile to game and roster data.

--- src/components/GamesList.js
+++ src/components/GamesList.js
@@ -10,7 +10,7 @@ const GamesList = function({games}) {
 
     return(
         <div className="games-list-component-div">
-            <h2>Played Games (Oldest &#8594; Newest)</h2>
+            <h2>Played Games (New &#8594; Old)</h2>
             <div className="games-div">
                 {gameItems}
             </div>
